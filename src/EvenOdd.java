f3e45435683feb65c058143e25335cfb43f6f7c4
AndersGraa <andersgraa@gmail.com>
2026-09-21 14:59:08 +0200

    Added new isEven method to fix bug.

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -2,9 +2,12 @@
 
 void main() {
     IO.println("Tallet 3 er ulige: " + isOdd(3));
-    IO.println("Tallet 4 er lige: " + isOdd(4));
+    IO.println("Tallet 4 er lige: " + isEven(4));
 }
 
 boolean isOdd(int number) {
     return number / 2 == 1;
+}
+boolean isEven(int number){
+    return number / 2 != 1;
 }
